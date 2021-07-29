7bd97db83d7b568aacca3a78ca5ece1f3e728f5e
partharoyy <partharoyy88@gmail.com>
2021-07-29 17:34:20 +0530

    disabled firebase for now

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase'
-
+import firebase from "firebase";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDiCc3Ixqdkjm0nTua2FyEKCAVXwFByUGk",
-    authDomain: "quickposts-a4a72.firebaseapp.com",
-    projectId: "quickposts-a4a72",
-    storageBucket: "quickposts-a4a72.appspot.com",
-    messagingSenderId: "253556249416",
-    appId: "1:253556249416:web:0d550fbc4594c6ab5b197d"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDiCc3Ixqdkjm0nTua2FyEKCAVXwFByUGk",
+//     authDomain: "quickposts-a4a72.firebaseapp.com",
+//     projectId: "quickposts-a4a72",
+//     storageBucket: "quickposts-a4a72.appspot.com",
+//     messagingSenderId: "253556249416",
+//     appId: "1:253556249416:web:0d550fbc4594c6ab5b197d"
+// };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
@@ -18,4 +17,4 @@ const storage = firebase.storage();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, storage, auth, provider }
+export { db, storage, auth, provider };
